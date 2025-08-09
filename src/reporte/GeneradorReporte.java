df4d64a3db9426d0386ea6b2e930736510113769
Lynkzy1 <pmedagl87645@universidadean.edu.co>
2025-08-09 17:44:07 -0500

    Create GeneradorReporte.java

---
+++ src/reporte/GeneradorReporte.java
@@ -0,0 +1,11 @@
+package reporte;
+
+/**
+ * Clase encargada de generar reportes de la feria.
+ */
+public class GeneradorReporte {
+    public void generar() {
+        System.out.println("Generando reporte...");
+        // Implementación futura
+    }
+}
