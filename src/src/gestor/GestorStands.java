414ee3c036f0d1f445b5775638b93e324169f1a8
Lynkzy1 <pmedagl87645@universidadean.edu.co>
2025-08-09 18:55:10 -0500

    Update GestorStands.java

--- src/src/gestor/GestorStands.java
+++ src/src/gestor/GestorStands.java
@@ -1,6 +1,7 @@
 package gestor;
 
 import modelo.Stand;
+import modelo.Empresa;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +15,46 @@ public class GestorStands {
         stands.add(stand);
     }
 
-    public List<Stand> getStands() {
+    public Stand buscarPorNumero(String numero) {
+        for (Stand s : stands) {
+            if (s.getNumero().equalsIgnoreCase(numero)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public void asignarStand(String numero, Empresa empresa) {
+        Stand s = buscarPorNumero(numero);
+        if (s != null && s.estaDisponible()) {
+            s.setEmpresaAsignada(empresa);
+        }
+    }
+
+    public void liberarStand(String numero) {
+        Stand s = buscarPorNumero(numero);
+        if (s != null) {
+            s.setEmpresaAsignada(null);
+        }
+    }
+
+    public List<Stand> listarDisponibles() {
+        List<Stand> disponibles = new ArrayList<>();
+        for (Stand s : stands) {
+            if (s.estaDisponible()) disponibles.add(s);
+        }
+        return disponibles;
+    }
+
+    public List<Stand> listarOcupados() {
+        List<Stand> ocupados = new ArrayList<>();
+        for (Stand s : stands) {
+            if (!s.estaDisponible()) ocupados.add(s);
+        }
+        return ocupados;
+    }
+
+    public List<Stand> listarStands() {
         return stands;
     }
 }
