1a10ed90abac25079495315c08250ee451d91b2b
Lynkzy1 <pmedagl87645@universidadean.edu.co>
2025-08-09 17:33:42 -0500

    Create FeriaEmpresarial.java

---
+++ src/feria/FeriaEmpresarial.java
@@ -0,0 +1,10 @@
+package feria;
+
+/**
+ * Clase principal de la aplicación Feria Empresarial.
+ */
+public class FeriaEmpresarial {
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenido a la Feria Empresarial!");
+    }
+}
